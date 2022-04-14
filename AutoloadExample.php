ec7bd36bdeb94fdabd8f57b064211e8e17269e69
budipratama <budi.pratama@icode.co.id>
2022-04-15 05:57:16 +0700

    first commit

---
+++ AutoloadExample.php
@@ -0,0 +1,13 @@
+<?php
+namespace budipratama\user;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
